0fc21301687a598cc18aefb3c9a4945e3edb79b5
chaykinks <chaykinks@gmail.com>
2024-10-08 19:10:01 +0500

    Added userCreatesTwoBuildTypesWithTheSameIdTest

--- src/test/java/org/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/org/example/teamcity/api/BuildTypeTest.java
@@ -1,10 +1,13 @@
 package org.example.teamcity.api;
 
+import org.apache.http.HttpStatus;
 import org.example.teamcity.api.models.BuildType;
 import org.example.teamcity.api.models.Project;
 import org.example.teamcity.api.models.User;
 import org.example.teamcity.api.requests.CheckedRequests;
+import org.example.teamcity.api.requests.unchecked.UncheckedBase;
 import org.example.teamcity.api.spec.Specifications;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -71,11 +74,30 @@ public class BuildTypeTest extends BaseApiTest {
 
     @Test(description = "User should not be able to create two build types with the same id", groups = {"Negative", "CRUD"})
     public void userCreatesTwoBuildTypesWithTheSameIdTest() {
-        step("Create user");
+        var user = generate(User.class);
+
+        superUserCheckRequests.getRequest(USERS).create(user);
+        var userCheckRequests = new CheckedRequests(Specifications.authorizedSpec(user));
+
+        var project = generate(Project.class);
+
+        project = userCheckRequests.<Project>getRequest(PROJECTS).create(project);
+
+        var buildType1 = generate(Arrays.asList(project), BuildType.class);
+        var buildType2 = generate(Arrays.asList(project), BuildType.class, buildType1.getId());
+
+        userCheckRequests.getRequest(BUILD_TYPES).create(buildType1);
+        new UncheckedBase(Specifications.authorizedSpec(user), BUILD_TYPES)
+                .create(buildType2)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString(("The build configuration / template ID \"%s\" is already used by another " +
+                        "configuration or template").formatted(buildType1.getId())));
+
+        /*step("Create user");
         step("Create project by user");
         step("Create buildType1 for project by user");
         step("Create buildType2 with same id as buildType1 for project by user");
-        step("Check buildType2 was not created with bad request code");
+        step("Check buildType2 was not created with bad request code");*/
     }
 
     @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive", "Roles"})
