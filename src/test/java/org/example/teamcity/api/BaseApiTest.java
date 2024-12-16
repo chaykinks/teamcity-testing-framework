1ce0bbdafc0214f771765cd6f69ee5c16291c0aa
chaykinks <chaykinks@gmail.com>
2024-12-16 19:23:51 +0500

    add ui tests

--- src/test/java/org/example/teamcity/api/BaseApiTest.java
+++ src/test/java/org/example/teamcity/api/BaseApiTest.java
@@ -1,4 +1,6 @@
 package org.example.teamcity.api;
 
+import org.example.teamcity.BaseTest;
+
 public class BaseApiTest extends BaseTest {
 }
--- src/test/java/org/example/teamcity/ui/CreateProjectTest.java
+++ src/test/java/org/example/teamcity/ui/CreateProjectTest.java
@@ -1,4 +1,11 @@
 package org.example.teamcity.ui;
 
-public class CreateProjectTest {
+import org.testng.annotations.Test;
+
+
+public class CreateProjectTest extends BaseUiTest {
+
+    @Test(description = "User should be able to create project", groups = {"Regression"})
+    public void userCreatesProjectTest() {
+    }
 }
