ee0f9aff02f5650f179064bba1efbe5d131f9285
chaykinks <chaykinks@gmail.com>
2024-12-18 19:14:15 +0500

    add selenide dependency and BaseUiTest config

--- src/test/java/org/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/org/example/teamcity/ui/BaseUiTest.java
@@ -1,4 +1,29 @@
 package org.example.teamcity.ui;
 
-public class BaseUiTest {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.example.teamcity.BaseTest;
+import org.example.teamcity.api.config.Config;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.Map;
+
+public class BaseUiTest extends BaseTest {
+    @BeforeSuite(alwaysRun = true)
+    public void setupUiTest() {
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // НЕТ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
+        // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
+        Configuration.remote = Config.getProperty("remote");
+        Configuration.browserSize = Config.getProperty("browserSize");
+
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
 }
