c9a7ab525e30c05b6b0d09d9c2649e3e06fc539a
chaykinks <chaykinks@gmail.com>
2025-01-03 18:52:22 +0500

    added first start setup

---
+++ src/test/java/org/example/teamcity/ui/SetupServerTest.java
@@ -0,0 +1,11 @@
+package org.example.teamcity.ui;
+
+import org.example.teamcity.ui.setup.FirstStartPage;
+import org.testng.annotations.Test;
+
+public class SetupServerTest extends BaseUiTest {
+    @Test(groups = {"Setup"})
+    public void setupTeamCityServerTest(){
+        FirstStartPage.open().setupFirstStart();
+    }
+}
